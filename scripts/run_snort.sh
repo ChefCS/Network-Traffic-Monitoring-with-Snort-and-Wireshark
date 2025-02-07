17e2e33f8764bb8c86decd6479134aef87946579
Cesar <cesar.sandoval.israel@gmail.com>
2025-02-06 17:36:19 -0800

    Update run_snort.sh

--- scripts/run_snort.sh
+++ scripts/run_snort.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Starting Snort..."
-sudo /opt/homebrew/Cellar/snort/3.3.7.0/bin/snort -c /opt/homebrew/Cellar/snort/3.3.7.0/etc/snort/snort.lua -i en0
-
+sudo /opt/homebrew/Cellar/snort/3.3.7.0/bin/snort -c /opt/homebrew/Cellar/snort/3.3.7.0/etc/snort/snort.lua -i en0 | tee snort_output.log
